5893af7a6714f5c8d6bcf424d2ce62b6c46bfd11
Jonathan <jon.michael.edwards13@gmail.com>
2018-09-26 21:26:56 +1200

    three corner problem

--- labs/lab2-ex4/lab2-ex4.c
+++ labs/lab2-ex4/lab2-ex4.c
@@ -5,22 +5,60 @@
    You must have completed this before starting this lab2-exercise.  */
 #include "pacer.h"
 
+
+/* Define PIO pins driving LED matrix rows and columns.  */
+static pio_t ledmat_rows[] =
+{
+    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO, LEDMAT_ROW4_PIO,
+    LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO, LEDMAT_ROW7_PIO
+};
+static pio_t ledmat_cols[] =
+{
+    LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
+    LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
+};
+
+
+/** Initialise LED matrix PIO pins.  */
+static void ledmat_init(void) {
+    for (uint8_t row = 0; row < 7; row++) {
+        pio_config_set(ledmat_rows[row], PIO_OUTPUT_HIGH);
+    }
+
+    for (uint8_t col = 0; col < 5; col++) {
+        pio_config_set(ledmat_cols[col], PIO_OUTPUT_HIGH);
+    }
+}
+
+
 int main (void)
 {
     system_init ();
     
     /* TODO: Initialise the pins of the LED matrix.  */
-    
+    ledmat_init();
 
     /* Set up pacer with a frequency of 50 Hz.  */
-    pacer_init (50);
+    pacer_init(50);
 
     while (1)
-    {
-        /* Pace the loop.  */
-        pacer_wait ();
-        
-        /* TODO: Drive the LED matrix using the pio functions,
-           displaying only three corner LEDs.  */
+    {   
+        /* State 1 Set high and low outputs */
+        pio_output_high(LEDMAT_ROW1_PIO);
+        pio_output_high(LEDMAT_COL5_PIO);
+        pio_output_low(LEDMAT_ROW1_PIO); 
+        pio_output_low(LEDMAT_ROW7_PIO);
+        pio_output_low(LEDMAT_COL1_PIO); 
+       
+        // Wait for 50 Hz
+        pacer_wait();
+
+        /* State 2, Set high and low outputs */
+        pio_output_high(LEDMAT_ROW7_PIO);
+        pio_output_low(LEDMAT_ROW1_PIO); 
+        pio_output_low(LEDMAT_COL5_PIO); 
+
+        // Wait for 50 Hz
+        pacer_wait();
     }
 }
