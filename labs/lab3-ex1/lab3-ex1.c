f1b3c91fb02391e6fac3d55dbd2761bf38f84d30
Jonathan <jon.michael.edwards13@gmail.com>
2018-09-30 19:28:19 +1300

    exercise 1

--- labs/lab3-ex1/lab3-ex1.c
+++ labs/lab3-ex1/lab3-ex1.c
@@ -8,22 +8,27 @@
 #define MESSAGE_RATE 10
 
 int main (void)
-{
+{   
+    const char *string = "Hello World";
+
     system_init();
 
     /* TODO: Initialise tinygl. */
+    tinygl_init(PACER_RATE);
+    tinygl_font_set(&font5x7_1); 
+    tinygl_text_speed_set(MESSAGE_RATE);
+    tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
 
     /* TODO: Set the message using tinygl_tlab3-ext().  */
-    
+    tinygl_text(string);
 
     pacer_init (PACER_RATE);
 
-    while(1)
-    {
+    while(1) {
         pacer_wait();
         
         /* TODO: Call the tinygl update function. */
-        
+        tinygl_update();
     }
     return 0;
 }
