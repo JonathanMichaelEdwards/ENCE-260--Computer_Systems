c41bdfe64d9ff410c9b3ce7ae4fbf56eea208ce2
Jonathan <jon.michael.edwards13@gmail.com>
2018-10-02 22:43:04 +1300

    Makefile fix

---
+++ labs/Extra-J/pacer.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include "pacer.h"
+
+#define SEC_TO_MILL 1000
+#define TIMER_PRESCALE 1024  // 2^10
+#define DELAY_T(DELAY) (DELAY * (F_CPU / TIMER_PRESCALE) / SEC_TO_MILL)
+
+
+static uint16_t pacer_period;
+
+/* Initialise the pacer module.  */
+void pacer_init(uint16_t pacer_frequency)
+{
+    /* TODO: initialise timer/counter peripheral the
+       same way as in lab2/lab2-ex2/timer.c but also calculate 
+       the timer/counter value from the pacer frequency */
+    TCCR1A = 0x00; 
+    TCCR1B = 0x05; 
+    TCCR1C = 0x00;
+
+    pacer_period = DELAY_T(SEC_TO_MILL / pacer_frequency);
+}
+
+
+/* Pace a while loop.  */
+void pacer_wait (void)
+{      
+    while (TCNT1 < pacer_period) continue;
+    TCNT1 = 0;
+}
---
+++ labs/Extra-J/pacer.h
@@ -0,0 +1,14 @@
+#ifndef PACER_H
+#define PACER_H
+
+#include "system.h"
+#include "pio.h"
+
+/* Initialise the pacer module.  */
+void pacer_init (uint16_t pacer_frequency);
+
+
+/* Pace a while loop.  */
+void pacer_wait (void);
+
+#endif //PACER_H
